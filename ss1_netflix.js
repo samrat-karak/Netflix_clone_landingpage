4a07d650c17088ed4a67f75a5692a0eaf19d44d0
samrat-karak <160379620+samrat-karak@users.noreply.github.com>
2024-02-25 23:38:10 +0530

    Add files via upload

---
+++ ss1_netflix.js
@@ -0,0 +1,78 @@
+var x=document.getElementById('myvideo');
+x.autoplay=true
+x.load();
+
+function drop1() {
+    var cont = document.getElementById('fifthboxjs');
+    if (cont.style.display == 'block') {
+        cont.style.display = 'none';
+
+        document.getElementById(fifthboxjs).value = 'Hide DIV';
+    }
+    else {
+        cont.style.display = 'block';
+        document.getElementById(fifthboxjs).value = 'Show DIV';
+    }
+}
+function drop2() {
+    var cont = document.getElementById('fifthboxjs1');
+    if (cont.style.display == 'block') {
+        cont.style.display = 'none';
+
+        document.getElementById(fifthboxjs1).value = 'Hide DIV';
+    }
+    else {
+        cont.style.display = 'block';
+        document.getElementById(fifthboxjs1).value = 'Show DIV';
+    }
+}
+function drop3() {
+    var cont = document.getElementById('fifthboxjs2');
+    if (cont.style.display == 'block') {
+        cont.style.display = 'none';
+
+        document.getElementById(fifthboxjs2).value = 'Hide DIV';
+    }
+    else {
+        cont.style.display = 'block';
+        document.getElementById(fifthboxjs2).value = 'Show DIV';
+    }
+}
+function drop4() {
+    var cont = document.getElementById('fifthboxjs3');
+    if (cont.style.display == 'block') {
+        cont.style.display = 'none';
+
+        document.getElementById(fifthboxjs3).value = 'Hide DIV';
+    }
+    else {
+        cont.style.display = 'block';
+        document.getElementById(fifthboxjs3).value = 'Show DIV';
+    }
+}
+function drop5() {
+    var cont = document.getElementById('fifthboxjs4');
+    if (cont.style.display == 'block') {
+        cont.style.display = 'none';
+
+        document.getElementById(fifthboxjs4).value = 'Hide DIV';
+    }
+    else {
+        cont.style.display = 'block';
+        document.getElementById(fifthboxjs4).value = 'Show DIV';
+    }
+}
+function drop6() {
+    var cont = document.getElementById('fifthboxjs5');
+    if (cont.style.display == 'block') {
+        cont.style.display = 'none';
+
+        document.getElementById(fifthboxjs5).value = 'Hide DIV';
+    }
+    else {
+        cont.style.display = 'block';
+        document.getElementById(fifthboxjs5).value = 'Show DIV';
+        
+    }
+}
+
